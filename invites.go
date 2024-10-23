fd81bea8a9766237347a3f31723f9a8e15df48fc
Nick Klauer <klauer@gmail.com>
2024-10-23 18:59:39 -0500

    refactor: move CustomTime around and add function to return it

--- invites.go
+++ invites.go
@@ -5,35 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"time"
 )
-
-type CustomTime time.Time
-
-func (ct *CustomTime) UnmarshalJSON(b []byte) error {
-	if b[0] == '"' && b[len(b)-1] == '"' {
-		// Handle string format
-		t, err := time.Parse(time.RFC3339, string(b[1:len(b)-1]))
-		if err != nil {
-			return err
-		}
-		*ct = CustomTime(t)
-	} else {
-		// Handle numeric format (assume Unix timestamp in seconds)
-		var timestamp int64
-		err := json.Unmarshal(b, &timestamp)
-		if err != nil {
-			return err
-		}
-		*ct = CustomTime(time.Unix(timestamp, 0))
-	}
-	return nil
-}
-
-// Add this method to the CustomTime type
-func (ct CustomTime) String() string {
-	return time.Time(ct).Format(time.RFC3339)
-}
 
 type Invite struct {
 	ObjectType string      `json:"object"`
