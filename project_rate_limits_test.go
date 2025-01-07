5c4e513cb00bd8150a9be4c200827633e3041eab
Christopher Virtucio <christopher.virtucio@zendesk.com>
2025-01-07 16:28:39 -0500

    covering After query param

--- project_rate_limits_test.go
+++ project_rate_limits_test.go
@@ -58,6 +58,114 @@ func TestListProjectRateLimits(t *testing.T) {
 	h.assertRequest("GET", path, 1)
 }
 
+func TestListProjectRateLimitsPaginated(t *testing.T) {
+	h := newTestHelper(t)
+	defer h.cleanup()
+
+	// Mock response data
+	mockProjectRateLimits := []ProjectRateLimit{
+		{
+			Object:                      "project.rate_limit",
+			ID:                          "rl-babbage-002",
+			Model:                       "babbage-002",
+			MaxRequestsPer1Minute:       3,
+			MaxTokensPer1Minute:         150000,
+			MaxImagesPer1Minute:         10,
+			MaxAudioMegabytesPer1Minute: 0,
+			MaxRequestsPer1Day:          200,
+			Batch1DayMaxInputTokens:     0,
+		},
+	}
+
+	// Register mock response
+	response := ListResponse[ProjectRateLimit]{
+		Object:  "list",
+		Data:    mockProjectRateLimits,
+		FirstID: "rl-babbage-002",
+		LastID:  "rl-babbage-002",
+		HasMore: true,
+	}
+	projectId := "proj_123"
+	path := fmt.Sprintf("%s/%s/rate_limits", ProjectsListEndpoint, projectId)
+	h.mockResponse("GET", path, 200, response)
+
+	// Make the API call
+	projectRateLimits, err := h.client.ListProjectRateLimits(1, "", projectId)
+	// Assert results
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+		return
+	}
+	if len(projectRateLimits.Data) != 1 {
+		t.Errorf("Expected 1 project, got %d", len(projectRateLimits.Data))
+		return
+	}
+	if mockProjectRateLimits[0].ID != projectRateLimits.Data[0].ID {
+		t.Errorf("Expected ID %s, got %s", mockProjectRateLimits[0].ID, projectRateLimits.Data[0].ID)
+	}
+	if mockProjectRateLimits[0].Model != projectRateLimits.Data[0].Model {
+		t.Errorf("Expected Model %s, got %s", mockProjectRateLimits[0].Model, projectRateLimits.Data[0].Model)
+	}
+
+	if !projectRateLimits.HasMore {
+		t.Errorf("Expected there to be more project rate limits")
+		return
+	}
+
+	// Verify the request was made
+	h.assertRequest("GET", path, 1)
+
+	// Mock response data
+	mockProjectRateLimits = []ProjectRateLimit{
+		{
+			Object:                      "project.rate_limit",
+			ID:                          "rl-babbage-003",
+			Model:                       "babbage-002",
+			MaxRequestsPer1Minute:       3,
+			MaxTokensPer1Minute:         150000,
+			MaxImagesPer1Minute:         10,
+			MaxAudioMegabytesPer1Minute: 0,
+			MaxRequestsPer1Day:          200,
+			Batch1DayMaxInputTokens:     0,
+		},
+	}
+
+	// Register mock response
+	response = ListResponse[ProjectRateLimit]{
+		Object:  "list",
+		Data:    mockProjectRateLimits,
+		FirstID: "rl-babbage-003",
+		LastID:  "rl-babbage-003",
+		HasMore: false,
+	}
+	h.mockResponse("GET", path, 200, response)
+
+	projectRateLimits, err = h.client.ListProjectRateLimits(1, projectRateLimits.LastID, projectId)
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+		return
+	}
+	if len(projectRateLimits.Data) != 1 {
+		t.Errorf("Expected 1 project, got %d", len(projectRateLimits.Data))
+		return
+	}
+	if mockProjectRateLimits[0].ID != projectRateLimits.Data[0].ID {
+		t.Errorf("Expected ID %s, got %s", mockProjectRateLimits[0].ID, projectRateLimits.Data[0].ID)
+	}
+	if mockProjectRateLimits[0].Model != projectRateLimits.Data[0].Model {
+		t.Errorf("Expected Model %s, got %s", mockProjectRateLimits[0].Model, projectRateLimits.Data[0].Model)
+	}
+
+	if projectRateLimits.HasMore {
+		t.Errorf("Expected there to be no more project rate limits")
+		return
+	}
+
+	// Verify the request was made
+	h.assertRequest("GET", path, 1)
+}
+
 func TestModifyProjectRateLimits(t *testing.T) {
 	h := newTestHelper(t)
 	defer h.cleanup()
